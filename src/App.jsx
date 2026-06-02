0f5d4a3cab13220f33db181803ef75fc6756079d
Akaioka <brendanvillaraza@yahoo.com>
2026-06-02 11:13:15 -0700

    Added ButtonUsage component for testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import ButtonUsage from './ButtonUsage.jsx'
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 
   return (
     <>
+    <ButtonUsage />
      <h1>temp</h1>
     </>
   )
